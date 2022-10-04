c37ba6d9e968b650a718ccf2ff471d79928292c2
teddysmithdev <teddysmithdev@gmail.com>
2022-10-04 06:10:00 -0400

    Setup Spring Data

--- src/main/java/com/pokemonreview/api/models/Pokemon.java
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -1,9 +1,21 @@
 package com.pokemonreview.api.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String type;
