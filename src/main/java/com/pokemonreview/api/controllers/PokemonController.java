c61242bec6acdf174e3e3dfd805ee4e106f33a63
teddysmithdev <teddysmithdev@gmail.com>
2022-10-04 07:29:09 -0400

    Controllers

---
+++ src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -0,0 +1,25 @@
+package com.pokemonreview.api.controllers;
+
+
+import com.pokemonreview.api.models.Pokemon;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class PokemonController {
+
+    @GetMapping("pokemon")
+    public ResponseEntity<List<Pokemon>> getPokemons() {
+        List<Pokemon> pokemons = new ArrayList<>();
+        pokemons.add(new Pokemon(1, "Squirtle", "Water"));
+        pokemons.add(new Pokemon(2, "Pikachu", "Electric"));
+        pokemons.add(new Pokemon(3, "Charmander", "Fire"));
+        return ResponseEntity.ok(pokemons);
+    }
+}
