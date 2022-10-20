f09c134915665ad9c39894a3efb771a8d8a144a6
teddysmithdev <teddysmithdev@gmail.com>
2022-10-20 13:32:37 -0400

    SecurityFilterChain

---
+++ src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.pokemonreview.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
