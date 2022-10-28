50296efc526488b9d9f8104fedd84336488cb262
teddysmithdev <teddysmithdev@gmail.com>
2022-10-28 19:01:47 -0400

    First Unit Test

---
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class PokemonRepositoryTests {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @Test
+    public void PokemonRepository_SaveAll_ReturnSavedPokemon() {
+
+        //Arrange
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type("electric").build();
+
+        //Act
+        Pokemon savedPokemon = pokemonRepository.save(pokemon);
+
+        //Assert
+        Assertions.assertThat(savedPokemon).isNotNull();
+        Assertions.assertThat(savedPokemon.getId()).isGreaterThan(0);
+    }
+}
