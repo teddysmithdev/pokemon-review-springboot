e007e27bd031da694e73785e75331da647eeed18
teddysmithdev <teddysmithdev@gmail.com>
2022-11-13 05:47:56 -0500

    Service Layer

--- src/test/java/com/pokemonreview/api/service/PokemonServiceTests.java
+++ src/test/java/com/pokemonreview/api/service/PokemonServiceTests.java
@@ -15,6 +15,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +53,40 @@ public class PokemonServiceTests {
         PokemonResponse savePokemon = pokemonService.getAllPokemon(1,10);
 
         Assertions.assertThat(savePokemon).isNotNull();
+    }
+    @Test
+    public void PokemonService_FindById_ReturnPokemonDto() {
+        int pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder().id(1).name("pikachu").type("electric").type("this is a type").build();
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+
+        PokemonDto pokemonReturn = pokemonService.getPokemonById(pokemonId);
+
+        Assertions.assertThat(pokemonReturn).isNotNull();
+    }
+
+    @Test
+    public void PokemonService_UpdatePokemon_ReturnPokemonDto() {
+        int pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder().id(1).name("pikachu").type("electric").type("this is a type").build();
+        PokemonDto pokemonDto = PokemonDto.builder().id(1).name("pikachu").type("electric").type("this is a type").build();
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+        when(pokemonRepository.save(pokemon)).thenReturn(pokemon);
+
+        PokemonDto updateReturn = pokemonService.updatePokemon(pokemonDto, pokemonId);
+
+        Assertions.assertThat(updateReturn).isNotNull();
+    }
+
+    @Test
+    public void PokemonService_DeletePokemonById_ReturnVoid() {
+        int pokemonId = 1;
+        Pokemon pokemon = Pokemon.builder().id(1).name("pikachu").type("electric").type("this is a type").build();
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.ofNullable(pokemon));
+        doNothing().when(pokemonRepository).delete(pokemon);
+
+        assertAll(() -> pokemonService.deletePokemonId(pokemonId));
     }
 }
