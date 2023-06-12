f035b0a124562013b083819050a0ee3a8d3394e4
alex-boulay <alexandre.boulay59@gmail.com>
2023-06-12 15:42:23 +0200

    support for jwt11.5

--- src/main/java/com/pokemonreview/api/security/SecurityConstants.java
+++ src/main/java/com/pokemonreview/api/security/SecurityConstants.java
@@ -1,6 +1,12 @@
 package com.pokemonreview.api.security;
 
+import java.security.Key;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 public class SecurityConstants {
     public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "secret";
+	//private static final KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+	private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 }
